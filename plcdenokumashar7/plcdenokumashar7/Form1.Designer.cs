527e22098b911e7cebe0c105622c6e19bd00a44c
cemreakz <71972830+cemreakz@users.noreply.github.com>
2020-11-16 17:01:50 +0300

    Add files via upload
    S71200 Plcden C# ile data okuyup yazıldı.

---
+++ plcdenokumashar7/plcdenokumashar7/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace plcdenokumashar7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRead = new System.Windows.Forms.Button();
+            this.BtnWrite = new System.Windows.Forms.Button();
+            this.TxtRead1 = new System.Windows.Forms.TextBox();
+            this.TxtRead2 = new System.Windows.Forms.TextBox();
+            this.TxtRead3 = new System.Windows.Forms.TextBox();
+            this.TxtRead4 = new System.Windows.Forms.TextBox();
+            this.TxtWrite4 = new System.Windows.Forms.TextBox();
+            this.TxtWrite3 = new System.Windows.Forms.TextBox();
+            this.TxtWrite2 = new System.Windows.Forms.TextBox();
+            this.TxtWrite1 = new System.Windows.Forms.TextBox();
+            this.ErorText = new System.Windows.Forms.Label();
+            this.TxtRead5 = new System.Windows.Forms.TextBox();
+            this.TxtRead6 = new System.Windows.Forms.TextBox();
+            this.TxtWrite5 = new System.Windows.Forms.TextBox();
+            this.TxtWrite6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnRead
+            // 
+            this.BtnRead.Location = new System.Drawing.Point(97, 331);
+            this.BtnRead.Name = "BtnRead";
+            this.BtnRead.Size = new System.Drawing.Size(75, 23);
+            this.BtnRead.TabIndex = 0;
+            this.BtnRead.Text = "Read";
+            this.BtnRead.UseVisualStyleBackColor = true;
+            this.BtnRead.Click += new System.EventHandler(this.BtnRead_Click);
+            // 
+            // BtnWrite
+            // 
+            this.BtnWrite.Location = new System.Drawing.Point(471, 331);
+            this.BtnWrite.Name = "BtnWrite";
+            this.BtnWrite.Size = new System.Drawing.Size(75, 23);
+            this.BtnWrite.TabIndex = 1;
+            this.BtnWrite.Text = "Write";
+            this.BtnWrite.UseVisualStyleBackColor = true;
+            this.BtnWrite.Click += new System.EventHandler(this.BtnWrite_Click);
+            // 
+            // TxtRead1
+            // 
+            this.TxtRead1.Location = new System.Drawing.Point(97, 52);
+            this.TxtRead1.Name = "TxtRead1";
+            this.TxtRead1.Size = new System.Drawing.Size(100, 22);
+            this.TxtRead1.TabIndex = 2;
+            // 
+            // TxtRead2
+            // 
+            this.TxtRead2.Location = new System.Drawing.Point(97, 98);
+            this.TxtRead2.Name = "TxtRead2";
+            this.TxtRead2.Size = new System.Drawing.Size(100, 22);
+            this.TxtRead2.TabIndex = 3;
+            // 
+            // TxtRead3
+            // 
+            this.TxtRead3.Location = new System.Drawing.Point(97, 146);
+            this.TxtRead3.Name = "TxtRead3";
+            this.TxtRead3.Size = new System.Drawing.Size(100, 22);
+            this.TxtRead3.TabIndex = 4;
+            // 
+            // TxtRead4
+            // 
+            this.TxtRead4.Location = new System.Drawing.Point(97, 190);
+            this.TxtRead4.Name = "TxtRead4";
+            this.TxtRead4.Size = new System.Drawing.Size(100, 22);
+            this.TxtRead4.TabIndex = 5;
+            // 
+            // TxtWrite4
+            // 
+            this.TxtWrite4.Location = new System.Drawing.Point(471, 190);
+            this.TxtWrite4.Name = "TxtWrite4";
+            this.TxtWrite4.Size = new System.Drawing.Size(100, 22);
+            this.TxtWrite4.TabIndex = 9;
+            // 
+            // TxtWrite3
+            // 
+            this.TxtWrite3.Location = new System.Drawing.Point(471, 146);
+            this.TxtWrite3.Name = "TxtWrite3";
+            this.TxtWrite3.Size = new System.Drawing.Size(100, 22);
+            this.TxtWrite3.TabIndex = 8;
+            // 
+            // TxtWrite2
+            // 
+            this.TxtWrite2.Location = new System.Drawing.Point(471, 98);
+            this.TxtWrite2.Name = "TxtWrite2";
+            this.TxtWrite2.Size = new System.Drawing.Size(100, 22);
+            this.TxtWrite2.TabIndex = 7;
+            // 
+            // TxtWrite1
+            // 
+            this.TxtWrite1.Location = new System.Drawing.Point(471, 52);
+            this.TxtWrite1.Name = "TxtWrite1";
+            this.TxtWrite1.Size = new System.Drawing.Size(100, 22);
+            this.TxtWrite1.TabIndex = 6;
+            // 
+            // ErorText
+            // 
+            this.ErorText.AutoSize = true;
+            this.ErorText.Location = new System.Drawing.Point(104, 386);
+            this.ErorText.Name = "ErorText";
+            this.ErorText.Size = new System.Drawing.Size(46, 17);
+            this.ErorText.TabIndex = 10;
+            this.ErorText.Text = "label1";
+            // 
+            // TxtRead5
+            // 
+            this.TxtRead5.Location = new System.Drawing.Point(97, 232);
+            this.TxtRead5.Name = "TxtRead5";
+            this.TxtRead5.Size = new System.Drawing.Size(100, 22);
+            this.TxtRead5.TabIndex = 11;
+            // 
+            // TxtRead6
+            // 
+            this.TxtRead6.Location = new System.Drawing.Point(97, 270);
+            this.TxtRead6.Name = "TxtRead6";
+            this.TxtRead6.Size = new System.Drawing.Size(100, 22);
+            this.TxtRead6.TabIndex = 12;
+            // 
+            // TxtWrite5
+            // 
+            this.TxtWrite5.Location = new System.Drawing.Point(471, 232);
+            this.TxtWrite5.Name = "TxtWrite5";
+            this.TxtWrite5.Size = new System.Drawing.Size(100, 22);
+            this.TxtWrite5.TabIndex = 13;
+            // 
+            // TxtWrite6
+            // 
+            this.TxtWrite6.Location = new System.Drawing.Point(471, 270);
+            this.TxtWrite6.Name = "TxtWrite6";
+            this.TxtWrite6.Size = new System.Drawing.Size(100, 22);
+            this.TxtWrite6.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtWrite6);
+            this.Controls.Add(this.TxtWrite5);
+            this.Controls.Add(this.TxtRead6);
+            this.Controls.Add(this.TxtRead5);
+            this.Controls.Add(this.ErorText);
+            this.Controls.Add(this.TxtWrite4);
+            this.Controls.Add(this.TxtWrite3);
+            this.Controls.Add(this.TxtWrite2);
+            this.Controls.Add(this.TxtWrite1);
+            this.Controls.Add(this.TxtRead4);
+            this.Controls.Add(this.TxtRead3);
+            this.Controls.Add(this.TxtRead2);
+            this.Controls.Add(this.TxtRead1);
+            this.Controls.Add(this.BtnWrite);
+            this.Controls.Add(this.BtnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRead;
+        private System.Windows.Forms.Button BtnWrite;
+        private System.Windows.Forms.TextBox TxtRead1;
+        private System.Windows.Forms.TextBox TxtRead2;
+        private System.Windows.Forms.TextBox TxtRead3;
+        private System.Windows.Forms.TextBox TxtRead4;
+        private System.Windows.Forms.TextBox TxtWrite4;
+        private System.Windows.Forms.TextBox TxtWrite3;
+        private System.Windows.Forms.TextBox TxtWrite2;
+        private System.Windows.Forms.TextBox TxtWrite1;
+        private System.Windows.Forms.Label ErorText;
+        private System.Windows.Forms.TextBox TxtRead5;
+        private System.Windows.Forms.TextBox TxtRead6;
+        private System.Windows.Forms.TextBox TxtWrite5;
+        private System.Windows.Forms.TextBox TxtWrite6;
+    }
+}
+
---
+++ plcdenokumashar7/plcdenokumashar7/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sharp7;
+
+namespace plcdenokumashar7
+{
+    public partial class Form1 : Form
+    {
+        static S7Client Plc = new S7Client();
+        public int result = Plc.ConnectTo("192.168.0.1",0,1); //bağlanılacak plcnın ethernet adresi
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (result == 0)
+            {
+                ErorText.Text = "OK";
+            }
+            else
+            {
+                ErorText.Text = "Com Eror";
+            }
+        }
+
+        private void BtnRead_Click(object sender, EventArgs e)
+        {
+            var buffer = new byte[14];//guncelle
+            int readresult = Plc.DBRead(2, 0, buffer.Length, buffer);
+            TxtRead1.Text = Convert.ToString(S7.GetBitAt(buffer, 0, 0)); //0.byte'ın 0.biti
+            TxtRead2.Text = Convert.ToString(S7.GetBitAt(buffer, 0, 1)); //0.byte'ın 1.biti
+            TxtRead3.Text = Convert.ToString(S7.GetIntAt(buffer, 2)); // 2. byte (for plc 1int= 2 byte)
+            TxtRead4.Text = Convert.ToString(S7.GetUDIntAt(buffer, 4)); //4. byte (for plc 1dint= 4 byte)
+            TxtRead5.Text = Convert.ToString(S7.GetWordAt(buffer, 8)); //8.byte (1 word = 2 byte)
+            TxtRead6.Text = Convert.ToString(S7.GetDWordAt(buffer, 10)); //10.byte (1 dword = 4 byte) =10+4=14 oldu bufferda 14tü.
+
+        }
+
+        private void BtnWrite_Click(object sender, EventArgs e)
+        {
+            var buffer1 = new byte[14];//guncelle
+            S7.SetBitAt(ref buffer1, 0, 0, Convert.ToBoolean(TxtWrite1.Text)); //0.byte'ın 0.biti
+            S7.SetBitAt(ref buffer1, 0, 1, Convert.ToBoolean(TxtWrite2.Text)); //0.byte'ın 0.biti //0.byte'ın 0.biti
+            S7.SetIntAt(buffer1, 2, Convert.ToInt16(TxtWrite3.Text)); // 2. byte (for plc 1int= 2 byte)
+            S7.SetDIntAt(buffer1, 4, Convert.ToInt32(TxtWrite4.Text)); //4. byte (for plc 1dint= 4 byte)
+            S7.SetWordAt(buffer1, 8, Convert.ToUInt16(TxtWrite5.Text)); //8.byte (1 word = 2 byte)
+            S7.SetDWordAt(buffer1, 10, Convert.ToUInt32(TxtWrite6.Text)); //10.byte (1 dword = 4 byte) =10+4=14 oldu buffer1da 14tü.
+            int writeresult = Plc.DBWrite(2, 0, buffer1.Length, buffer1);
+        }
+    }
+
+}
